a4b057929d5f0e4417f9d3d72eae5a0d679a0408
muhammadyahya1 <81598272+muhammadyahya1@users.noreply.github.com>
2021-05-11 10:15:54 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>PHP Dasar</title>
+</head>
+<body>
+ <h1>Belajar PHP Dasar</h1>
+ <?php
+ echo "Hello World";
+ ?>
+</body>
+<h1>Menggunakan Variable</h1>
+<?php
+$nim = "311910814";
+$nama = 'Muhammad Yahya';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+</body>
+<h1>Predefine Variable</h1>
+<?php
+echo 'Selamat Datang ' . $_GET['Yahya'];
+?>
+</html>
+	
